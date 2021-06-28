5842f9997d6844d73135ef522b27cf496c5a5162
wangzilun <964606955@qq.com>
2021-06-28 19:53:58 +0800

    test

--- src/test/java/MybatisTest.java
+++ src/test/java/MybatisTest.java
@@ -1,3 +1,5 @@
+import com.mydemo.dao.UserDao;
+import com.mydemo.domain.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -5,6 +7,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class MybatisTest {
     public static void main(String[] args) throws IOException {
@@ -16,15 +19,19 @@ public class MybatisTest {
         SqlSessionFactory factory = builder.build(is);
 
         //3.使用工厂生产SqlSession对象
-        SqlSession session = factory.openSession();
+        SqlSession sqlSession = factory.openSession();
 
         //4.使用SqlSession创建Dao接口的代理对象
-
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
 
         //5.使用代理对象执行方法
-
+        List<User> users = userDao.findAll();
+        for (User user : users) {
+            System.out.println(user);
+        }
 
         //6.释放资源
-
+        sqlSession.close();
+        is.close();
     }
 }
